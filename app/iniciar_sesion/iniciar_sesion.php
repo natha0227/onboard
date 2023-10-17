045bec3f1fcb49fc0ee16c8eaf899acf196d745c
Jana Yuli\usuario <janayulicardenas10@gmail.com>
2023-10-17 00:09:01 -0500

    cositas que pasa

--- app/iniciar_sesion/iniciar_sesion.php
+++ app/iniciar_sesion/iniciar_sesion.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 include ("conexion.php");
-<<<<<<< HEAD
-error_reporting(0);
-=======
->>>>>>> 132b51384738e71c8d503c17b7a6fd839ab92621
 $mail = $_POST['mail'];
 $contrasena = $_POST['contrasena'];
 
@@ -20,9 +16,10 @@ $_SESSION['apellido']=$datos['apellido'];
 $_SESSION['id']=$datos['id'];
 $_SESSION['lvel']=$datos['lvel'];
 
+$subir = "UPDATE usuario SET estado = 1 WHERE id = $_SESSION[id]";
+$subido = mysqli_query($conn,$subir);
+
 if (mysqli_num_rows($result) > 0) {
-    $subir = "UPDATE usuario SET estado = 1 WHERE id = $_SESSION[id]";
-    $subido = mysqli_query($conn,$subir);
     switch ($_SESSION['lvel']) {
         case 1:
             echo'<script type="text/javascript">
@@ -46,23 +43,10 @@ if (mysqli_num_rows($result) > 0) {
             exit;
             break;            
     }
-} else if (mysqli_num_rows($result) == '0') {
-    $cons = "SELECT * FROM usuario WHERE mail='$mail' AND contrasena ='$contrasena'";
-    $result = mysqli_query($conn,$cons);
-    $datos=mysqli_fetch_assoc($result);
-    $id=$datos['id'];
-    $cerrar = "UPDATE usuario SET estado = 0 WHERE id = $id";
-    $cerrado = mysqli_query($conn,$cerrar);
-    echo '<script type="text/javascript">';
-    echo 'alert("Este usuario ya tiene una sesión iniciada o sus Credenciales son invalidas");';
-    echo 'window.location.href = "index.html";';
-    echo '</script>';
-exit;
+
 } else {
-    echo '<script type="text/javascript">';
-    echo 'alert("Credenciales invalidas");';
-    echo 'window.location.href = "index.html";';
-    echo '</script>';
+    echo "session iniciada o credenciales";
 }
+
 mysqli_close($conn);
 ?>
